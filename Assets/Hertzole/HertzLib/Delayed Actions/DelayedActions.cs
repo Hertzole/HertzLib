71b5a651053e11e77917e56b3fc4ded99b355152
Hertzole <hampusringstrom@gmail.com>
2018-12-27 00:54:17 +0100

    Changed the structure of Delayed Actions and Update Manager.
    They are now supposed to be a lot more straight forward by removing all the MonoBehaviour methods.

--- Assets/Hertzole/HertzLib/Delayed Actions/DelayedActions.cs
+++ Assets/Hertzole/HertzLib/Delayed Actions/DelayedActions.cs
@@ -4,95 +4,105 @@ using UnityEngine;
 
 namespace Hertzole.HertzLib
 {
-#if HERTZLIB_UPDATE_MANAGER
-    [DisallowMultipleComponent]
-    [HelpURL("https://github.com/Hertzole/HertzLib/wiki/Delayed-Actions")]
-    public class DelayedActions : MonoBehaviour, ILateUpdate
-#else
-    public class DelayedActions : MonoBehaviour
-#endif
+    public static class DelayedActions
     {
-        private struct ActionInfo
+#if HERTZLIB_UPDATE_MANAGER
+        [DisallowMultipleComponent]
+        [HelpURL("https://github.com/Hertzole/HertzLib/wiki/Delayed-Actions")]
+        internal class DelayedActionsBehaviour : MonoBehaviour, ILateUpdate
+#else
+        internal class DelayedActionsBehaviour : MonoBehaviour
+#endif
         {
-            public float time;
-            public Action action;
-
-            public ActionInfo(float time, Action action)
+            private struct ActionInfo
             {
-                this.time = time;
-                this.action = action;
-            }
-        }
+                public float time;
+                public Action action;
 
-        private static DelayedActions instance;
-        private static DelayedActions Instance
-        {
-            get
-            {
-                if (!instance)
+                public ActionInfo(float time, Action action)
                 {
-                    instance = FindObjectOfType<DelayedActions>();
+                    this.time = time;
+                    this.action = action;
+                }
+            }
 
-                    if (instance == null)
+            private static DelayedActionsBehaviour instance;
+            public static DelayedActionsBehaviour Instance
+            {
+                get
+                {
+                    if (!instance)
                     {
                         GameObject go = new GameObject("Delayed Action Manager");
-                        instance = go.AddComponent<DelayedActions>();
+                        instance = go.AddComponent<DelayedActionsBehaviour>();
                         DontDestroyOnLoad(go);
                     }
+
+                    return instance;
                 }
-
-                return instance;
             }
-        }
 
-        // Sets if the object is in the process of being destroyed.
-        private static bool m_Destroying = false;
+            // Sets if the object is in the process of being destroyed.
+            private static bool m_Destroying = false;
 
-        // All the actions.
-        private List<ActionInfo> m_Actions = new List<ActionInfo>();
-        private static List<ActionInfo> Actions { get { return Instance.m_Actions; } }
+            // All the actions.
+            private List<ActionInfo> m_Actions = new List<ActionInfo>();
+            private static List<ActionInfo> Actions { get { return Instance.m_Actions; } }
 
-        private void OnDestroy()
-        {
-            // Mark the object as destroyed.
-            m_Destroying = true;
-        }
+            private void OnDestroy()
+            {
+                // Mark the object as destroyed.
+                m_Destroying = true;
+            }
 
-        private void OnApplicationQuit()
-        {
-            // Mark the object as destroyed.
-            m_Destroying = true;
-        }
+            private void OnApplicationQuit()
+            {
+                // Mark the object as destroyed.
+                m_Destroying = true;
+            }
 
 #if HERTZLIB_UPDATE_MANAGER
-        private void OnEnable()
-        {
-            UpdateManager.AddLateUpdate(this);
-        }
+            private void OnEnable()
+            {
+                UpdateManager.AddLateUpdate(this);
+            }
 
-        private void OnDisable()
-        {
-            UpdateManager.RemoveLateUpdate(this);
-        }
+            private void OnDisable()
+            {
+                UpdateManager.RemoveLateUpdate(this);
+            }
 
-        public void OnLateUpdate()
+            public void OnLateUpdate()
 #else
         private void LateUpdate()
 #endif
-        {
-            // As long as this object isn't getting destroyed, run the check.
-            if (!m_Destroying)
             {
-                // Reverse loop through the actions.
-                for (int i = m_Actions.Count - 1; i >= 0; i--)
+                // As long as this object isn't getting destroyed, run the check.
+                if (!m_Destroying)
                 {
-                    // If the time is over the action's time, invoke it and then remove it.
-                    if (Time.time >= m_Actions[i].time)
+                    // Reverse loop through the actions.
+                    for (int i = m_Actions.Count - 1; i >= 0; i--)
                     {
-                        m_Actions[i].action.Invoke();
-                        m_Actions.RemoveAt(i);
+                        // If the time is over the action's time, invoke it and then remove it.
+                        if (Time.time >= m_Actions[i].time)
+                        {
+                            m_Actions[i].action.Invoke();
+                            m_Actions.RemoveAt(i);
+                        }
                     }
                 }
+            }
+
+            /// <summary>
+            /// Schedules a new action.
+            /// </summary>
+            /// <param name="action">The action you want to execute.</param>
+            /// <param name="delay">The delay before the action is executed.</param>
+            public void ScheduleAction(Action action, float delay)
+            {
+                // If the object isn't being destroyed, add the action.
+                if (!m_Destroying)
+                    Actions.Add(new ActionInfo(Time.time + delay, action));
             }
         }
 
@@ -103,9 +113,16 @@ namespace Hertzole.HertzLib
         /// <param name="delay">The delay before the action is executed.</param>
         public static void ScheduleAction(Action action, float delay)
         {
-            // If the object isn't being destroyed, add the action.
-            if (!m_Destroying)
-                Actions.Add(new ActionInfo(Time.time + delay, action));
+            // Make sure delayed actions isn't being used in the editor.
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning("Delayed Actions can not be used outside the game!");
+                return;
+            }
+
+            // Schedule a new action on the instance.
+            if (DelayedActionsBehaviour.Instance)
+                DelayedActionsBehaviour.Instance.ScheduleAction(action, delay);
         }
     }
 }
--- Assets/Hertzole/HertzLib/Update Manager/UpdateManager.cs
+++ Assets/Hertzole/HertzLib/Update Manager/UpdateManager.cs
@@ -18,95 +18,157 @@ namespace Hertzole.HertzLib
         void OnLateUpdate();
     }
 
-    [DisallowMultipleComponent]
-    [HelpURL("https://github.com/Hertzole/HertzLib/wiki/Update-Manager")]
-    public class UpdateManager : MonoBehaviour
+    public static class UpdateManager
     {
-        [SerializeField]
-        [Tooltip("If true, the object will persist between scenes.")]
-        private bool m_Persist = true;
-
-        /// <summary> If true, the object will persist between scenes. </summary>
-        public bool Persist { get { return m_Persist; } set { m_Persist = value; } }
-
-        private static UpdateManager instance;
-        private static UpdateManager Instance
+        [DisallowMultipleComponent]
+        [HelpURL("https://github.com/Hertzole/HertzLib/wiki/Update-Manager")]
+        internal class UpdateManagerBehaviour : MonoBehaviour
         {
-            get
-            {
-                if (!instance)
-                {
-                    instance = FindObjectOfType<UpdateManager>();
 
-                    if (instance == null)
+            private static UpdateManagerBehaviour instance;
+            public static UpdateManagerBehaviour Instance
+            {
+                get
+                {
+                    if (!instance)
                     {
                         GameObject go = new GameObject("Update Manager");
-                        instance = go.AddComponent<UpdateManager>();
-                        if (instance.Persist)
-                            DontDestroyOnLoad(go);
+                        instance = go.AddComponent<UpdateManagerBehaviour>();
+                        DontDestroyOnLoad(go);
                     }
+
+                    return instance;
                 }
-
-                return instance;
             }
-        }
 
-        // Sets if the object is in the process of being destroyed.
-        private static bool m_Destroying = false;
+            // Sets if the object is in the process of being destroyed.
+            private static bool m_Destroying = false;
 
-        private List<IFixedUpdate> m_FixedUpdateList = new List<IFixedUpdate>();
-        private static List<IFixedUpdate> FixedUpdateList { get { return Instance.m_FixedUpdateList; } set { Instance.m_FixedUpdateList = value; } }
-        private List<ILateUpdate> m_LateUpdateList = new List<ILateUpdate>();
-        private static List<ILateUpdate> LateUpdateList { get { return Instance.m_LateUpdateList; } set { Instance.m_LateUpdateList = value; } }
-        private List<IUpdate> m_UpdateList = new List<IUpdate>();
-        private static List<IUpdate> UpdateList { get { return Instance.m_UpdateList; } set { Instance.m_UpdateList = value; } }
+            private List<IFixedUpdate> m_FixedUpdateList = new List<IFixedUpdate>();
+            private static List<IFixedUpdate> FixedUpdateList { get { return Instance.m_FixedUpdateList; } set { Instance.m_FixedUpdateList = value; } }
+            private List<ILateUpdate> m_LateUpdateList = new List<ILateUpdate>();
+            private static List<ILateUpdate> LateUpdateList { get { return Instance.m_LateUpdateList; } set { Instance.m_LateUpdateList = value; } }
+            private List<IUpdate> m_UpdateList = new List<IUpdate>();
+            private static List<IUpdate> UpdateList { get { return Instance.m_UpdateList; } set { Instance.m_UpdateList = value; } }
 
-        // Awake is called when the script instance is being loaded
-        private void Awake()
-        {
-            if (m_Persist)
-                DontDestroyOnLoad(gameObject);
-        }
-
-        private void OnDestroy()
-        {
-            // Mark the object as destroyed.
-            m_Destroying = true;
-        }
-
-        private void OnApplicationQuit()
-        {
-            // Mark the object as destroyed.
-            m_Destroying = true;
-        }
-
-        // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
-        private void FixedUpdate()
-        {
-            if (m_FixedUpdateList.Count > 0)
+            private void OnDestroy()
             {
-                for (int i = 0; i < m_FixedUpdateList.Count; i++)
-                    m_FixedUpdateList[i].OnFixedUpdate();
+                // Mark the object as destroyed.
+                m_Destroying = true;
             }
-        }
 
-        // LateUpdate is called every frame, if the Behaviour is enabled
-        private void LateUpdate()
-        {
-            if (m_LateUpdateList.Count > 0)
+            private void OnApplicationQuit()
             {
-                for (int i = 0; i < m_LateUpdateList.Count; i++)
-                    m_LateUpdateList[i].OnLateUpdate();
+                // Mark the object as destroyed.
+                m_Destroying = true;
             }
-        }
 
-        // Update is called every frame, if the MonoBehaviour is enabled
-        private void Update()
-        {
-            if (m_UpdateList.Count > 0)
+            // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
+            private void FixedUpdate()
             {
-                for (int i = 0; i < m_UpdateList.Count; i++)
-                    m_UpdateList[i].OnUpdate();
+                if (m_FixedUpdateList.Count > 0)
+                {
+                    for (int i = 0; i < m_FixedUpdateList.Count; i++)
+                        m_FixedUpdateList[i].OnFixedUpdate();
+                }
+            }
+
+            // LateUpdate is called every frame, if the Behaviour is enabled
+            private void LateUpdate()
+            {
+                if (m_LateUpdateList.Count > 0)
+                {
+                    for (int i = 0; i < m_LateUpdateList.Count; i++)
+                        m_LateUpdateList[i].OnLateUpdate();
+                }
+            }
+
+            // Update is called every frame, if the MonoBehaviour is enabled
+            private void Update()
+            {
+                if (m_UpdateList.Count > 0)
+                {
+                    for (int i = 0; i < m_UpdateList.Count; i++)
+                        m_UpdateList[i].OnUpdate();
+                }
+            }
+
+            /// <summary>
+            /// Adds a new component that needs to be called every frame.
+            /// </summary>
+            /// <param name="update"></param>
+            public void AddUpdate(IUpdate update)
+            {
+                // Don't do anything if the object will be destroyed.
+                if (m_Destroying)
+                    return;
+
+                UpdateList.Add(update);
+            }
+
+            /// <summary>
+            /// Removes a new that needs to be called every frame.
+            /// </summary>
+            /// <param name="update"></param>
+            public void RemoveUpdate(IUpdate update)
+            {
+                // Don't do anything if the object will be destroyed.
+                if (m_Destroying)
+                    return;
+
+                UpdateList.Remove(update);
+            }
+
+            /// <summary>
+            /// Adds a new component that needs to be called every fixed framerate frame.
+            /// </summary>
+            /// <param name="update"></param>
+            public void AddFixedUpdate(IFixedUpdate update)
+            {
+                // Don't do anything if the object will be destroyed.
+                if (m_Destroying)
+                    return;
+
+                FixedUpdateList.Add(update);
+            }
+
+            /// <summary>
+            /// Removes a new that needs to be called every fixed framerate frame.
+            /// </summary>
+            /// <param name="update"></param>
+            public void RemoveFixedUpdate(IFixedUpdate update)
+            {
+                // Don't do anything if the object will be destroyed.
+                if (m_Destroying)
+                    return;
+
+                FixedUpdateList.Remove(update);
+            }
+
+            /// <summary>
+            /// Adds a new component that needs to be called every frame.
+            /// </summary>
+            /// <param name="update"></param>
+            public void AddLateUpdate(ILateUpdate update)
+            {
+                // Don't do anything if the object will be destroyed.
+                if (m_Destroying)
+                    return;
+
+                LateUpdateList.Add(update);
+            }
+
+            /// <summary>
+            /// Removes a new that needs to be called every frame.
+            /// </summary>
+            /// <param name="update"></param>
+            public void RemoveLateUpdate(ILateUpdate update)
+            {
+                // Don't do anything if the object will be destroyed.
+                if (m_Destroying)
+                    return;
+
+                LateUpdateList.Remove(update);
             }
         }
 
@@ -116,11 +178,16 @@ namespace Hertzole.HertzLib
         /// <param name="update"></param>
         public static void AddUpdate(IUpdate update)
         {
-            // Don't do anything if the object will be destroyed.
-            if (m_Destroying)
+            // Make sure the Update Manager isn't being used in the editor.
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning("Update Manager can not be used outside the game!");
                 return;
+            }
 
-            UpdateList.Add(update);
+            // Add the update on the instance.
+            if (UpdateManagerBehaviour.Instance)
+                UpdateManagerBehaviour.Instance.AddUpdate(update);
         }
 
         /// <summary>
@@ -129,11 +196,16 @@ namespace Hertzole.HertzLib
         /// <param name="update"></param>
         public static void RemoveUpdate(IUpdate update)
         {
-            // Don't do anything if the object will be destroyed.
-            if (m_Destroying)
+            // Make sure the Update Manager isn't being used in the editor.
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning("Update Manager can not be used outside the game!");
                 return;
+            }
 
-            UpdateList.Remove(update);
+            // Remove the update on the instance.
+            if (UpdateManagerBehaviour.Instance)
+                UpdateManagerBehaviour.Instance.RemoveUpdate(update);
         }
 
         /// <summary>
@@ -142,11 +214,16 @@ namespace Hertzole.HertzLib
         /// <param name="update"></param>
         public static void AddFixedUpdate(IFixedUpdate update)
         {
-            // Don't do anything if the object will be destroyed.
-            if (m_Destroying)
+            // Make sure the Update Manager isn't being used in the editor.
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning("Update Manager can not be used outside the game!");
                 return;
+            }
 
-            FixedUpdateList.Add(update);
+            // Add the update on the instance.
+            if (UpdateManagerBehaviour.Instance)
+                UpdateManagerBehaviour.Instance.AddFixedUpdate(update);
         }
 
         /// <summary>
@@ -155,11 +232,16 @@ namespace Hertzole.HertzLib
         /// <param name="update"></param>
         public static void RemoveFixedUpdate(IFixedUpdate update)
         {
-            // Don't do anything if the object will be destroyed.
-            if (m_Destroying)
+            // Make sure the Update Manager isn't being used in the editor.
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning("Update Manager can not be used outside the game!");
                 return;
+            }
 
-            FixedUpdateList.Remove(update);
+            // Remove the update on the instance.
+            if (UpdateManagerBehaviour.Instance)
+                UpdateManagerBehaviour.Instance.RemoveFixedUpdate(update);
         }
 
         /// <summary>
@@ -168,11 +250,16 @@ namespace Hertzole.HertzLib
         /// <param name="update"></param>
         public static void AddLateUpdate(ILateUpdate update)
         {
-            // Don't do anything if the object will be destroyed.
-            if (m_Destroying)
+            // Make sure the Update Manager isn't being used in the editor.
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning("Update Manager can not be used outside the game!");
                 return;
+            }
 
-            LateUpdateList.Add(update);
+            // Add the update on the instance.
+            if (UpdateManagerBehaviour.Instance)
+                UpdateManagerBehaviour.Instance.AddLateUpdate(update);
         }
 
         /// <summary>
@@ -181,11 +268,16 @@ namespace Hertzole.HertzLib
         /// <param name="update"></param>
         public static void RemoveLateUpdate(ILateUpdate update)
         {
-            // Don't do anything if the object will be destroyed.
-            if (m_Destroying)
+            // Make sure the Update Manager isn't being used in the editor.
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning("Update Manager can not be used outside the game!");
                 return;
+            }
 
-            LateUpdateList.Remove(update);
+            // Remove the update on the instance.
+            if (UpdateManagerBehaviour.Instance)
+                UpdateManagerBehaviour.Instance.RemoveLateUpdate(update);
         }
     }
 }
